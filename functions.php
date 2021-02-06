6cdd94d0af770c19f5eabdc47bea7786f3fd2d65
Enrico <development@langhe.net>
2021-02-06 09:52:36 +0100

    stuff

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 function theme_enqueue_child_style(){
-	wp_enqueue_style('theme-child-style', get_stylesheet_directory_uri(). '/style.css',array('theme-style','theme-skin'),false,'all');
+	wp_enqueue_style('theme-child-style', get_stylesheet_directory_uri(). '/style.css', array('theme-style','theme-skin'),false,'all');
 }
 add_action('wp_print_styles', 'theme_enqueue_child_style', 20);
 
@@ -13,3 +13,18 @@ function striking_child_name() {
     ) );   
 }
 add_action('admin_bar_menu', 'striking_child_name',82);
+
+add_filter( 'gform_field_value_prezzo', 'duchessa_prezzo' );
+function duchessa_prezzo( $value ) {
+    global $post;
+    $prezzo = get_post_meta($post->ID, 'prezzo', true);
+
+    return $prezzo;
+}
+add_filter( 'gform_field_value_nome', 'duchessa_nome' );
+function duchessa_nome( $value ) {
+    global $post;
+    $nome = $post->post_title;
+
+    return $nome;
+}
